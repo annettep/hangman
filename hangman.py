2d2df104bcef745be83d98edf7ccc049cadde396
Annette Pan <annette@Annettes-MacBook-Pro.local>
2020-12-12 16:55:29 -0500

    third draft

--- hangman.py
+++ hangman.py
@@ -17,8 +17,15 @@ def main(): #overall program, need for restarting ability
             exit()
         else:
             print("Invalid input.")
+            
 
     word = input("Enter the word to be guessed: ") #replace with random word generator
+    if len(word) > 10: #difficulty based on amount of letters
+        difficulty = 8
+    elif len(word) > 5:
+        difficulty = 6
+    else:
+        difficulty = 4
 
     hidden_word_list = [] #creates same as word list except letters replaced with underlines
     for i in range(len(word)):
@@ -26,37 +33,53 @@ def main(): #overall program, need for restarting ability
     print(' '.join(hidden_word_list)) 
 
     word_list = list(word) #turns word into list of its letters
-    wrong_letters = []
-    #duplicate_letters = []
+    wrong_letters = [] #list of guessed wrong list
+    duplicate_letters = [] #list of letters already guess, right or wrong
+
+    
+
     def check_guess(char_guess): #creates function to check letter guessed
-        if char_guess in word_list: #if user guesses a correct letter
+        if char_guess in duplicate_letters: #if letter is already guessed
+            char_guess = input("You have already guessed this letter. Guess again: ") #prompts user to guess again
+
+        if char_guess in word_list and char_guess not in duplicate_letters: #if user guesses a correct letter
             for i in range(len(word_list)): #iterate through list
                 if char_guess == word_list[i]: #if guessed letter matches letter in word list
                     hidden_word_list[i] = word_list[i] #then replaces underline with correct letter
-                    #duplicate_letters.append(word_list[i])
-            
-            print(' '.join(hidden_word_list))
-        else: #if user does not guess correct letter
+                    duplicate_letters.append(word_list[i]) #adds letter to list of guessed letters
+            print(' '.join(hidden_word_list)) #displays word w/ respective underlines and letters
+
+        elif char_guess in duplicate_letters: #else if letter is already guessed
+            check_guess(char_guess) #then go through function again
+
+        else: #else if the letter has not been guessed and is wrong
             global count 
             count += 1 #increase hangman count
-            wrong_letters.append(char_guess)
-            print("Wrong letters:", ', '.join(wrong_letters))
-            print("You have", 6 - count, "wrong tries left.")
-            if count < 6: #checks if hangman still alive
-                char_guess = input("Wrong guess, try again: ") #prompts user to guess another letter
+
+            if count < difficulty: #if user is still in the game
+                wrong_letters.append(char_guess) #add letter to wrong letters list
+                duplicate_letters.append(char_guess) #add letter to guessed letters list
+                print("Wrong letters:", ', '.join(wrong_letters))
+                print("You have", difficulty - count, "wrong tries left.")
+                char_guess = input("Try again: ") #prompts user to guess another letter
                 check_guess(char_guess) #goes through function again, checking if letter is right or wrong
             else: #user loses
                 print("You lost... the word was", word)
-                restart()
+                restart() 
+
+        
+            
         
     global count
-    while "_" in hidden_word_list and count < 6: #if there are still unguessed letters and user has not reached hangman
+    while "_" in hidden_word_list and count < difficulty: #if there are still unguessed letters and user has not reached hangman
         char_guess = input("Guess a letter: ") #keep on guessing 
         check_guess(char_guess) #checks letter guessed
 
-    if count < 6 and "_" not in hidden_word_list: #if user wins
+    if count < difficulty and "_" not in hidden_word_list: #if user wins
         print("You win!")
         restart()
+    
+
         
 
 count = 0
