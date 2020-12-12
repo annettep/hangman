42e01d3c80096dfd422ecf829ab4e618e007dd2c
Annette Pan <annette@Annettes-MacBook-Pro.local>
2020-12-11 19:30:22 -0500

    second draft

--- hangman.py
+++ hangman.py
@@ -4,7 +4,20 @@
 # # Return a single random word
 # word = r.get_random_word()
 
-def main():
+def main(): #overall program, need for restarting ability
+
+    def restart(): #creates function to check if user wants to play again or not
+        global count
+        play_again = input("Would you like to play again? Enter y for yes or n for no. ")
+        if play_again == "y":
+            count = 0 
+            main()
+        elif play_again == "n":
+            print("Thanks for playing!")
+            exit()
+        else:
+            print("Invalid input.")
+
     word = input("Enter the word to be guessed: ") #replace with random word generator
 
     hidden_word_list = [] #creates same as word list except letters replaced with underlines
@@ -12,16 +25,16 @@ def main():
         hidden_word_list.append("_")
     print(' '.join(hidden_word_list)) 
 
-    #count = 0 #represents hangman
-
     word_list = list(word) #turns word into list of its letters
     wrong_letters = []
-    duplicate_letters = []
+    #duplicate_letters = []
     def check_guess(char_guess): #creates function to check letter guessed
         if char_guess in word_list: #if user guesses a correct letter
             for i in range(len(word_list)): #iterate through list
                 if char_guess == word_list[i]: #if guessed letter matches letter in word list
                     hidden_word_list[i] = word_list[i] #then replaces underline with correct letter
+                    #duplicate_letters.append(word_list[i])
+            
             print(' '.join(hidden_word_list))
         else: #if user does not guess correct letter
             global count 
@@ -34,15 +47,7 @@ def main():
                 check_guess(char_guess) #goes through function again, checking if letter is right or wrong
             else: #user loses
                 print("You lost... the word was", word)
-                play_again = input("Would you like to play again? Enter y for yes or n for no. ")
-                if play_again == "y":
-                    count = 0 
-                    main()
-                elif play_again == "n":
-                    print("Thanks for playing!")
-                    exit()
-                else:
-                    print("Invalid input.")
+                restart()
         
     global count
     while "_" in hidden_word_list and count < 6: #if there are still unguessed letters and user has not reached hangman
@@ -51,15 +56,7 @@ def main():
 
     if count < 6 and "_" not in hidden_word_list: #if user wins
         print("You win!")
-        play_again = input("Would you like to play again? Enter y for yes or n for no. ")
-        if play_again == "y":
-            count = 0 
-            main()
-        elif play_again == "n":
-            print("Thanks for playing!")
-            exit()
-        else:
-            print("Invalid input.")
+        restart()
         
 
 count = 0
